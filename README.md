e607e342bf849185b46595147353609951d6e9ed
Lewis Murray <lewisdmurray00@gmail.com>
2022-06-30 21:51:31 +0100

    Created a components folder and added all the 'main' components i think i might need

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## About this project
 - My idea here is to create a portfolio as a showcase of my work which ideally will co-insign with my CV
 
+## My project journey
+
+- I first started this project by doing the set-up stuff first, i had mocked up ideas of components i wanted and added them into a seperate folder, and focused on getting them set up and displayed on the screen
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Header from './Components/Header/Header'
+import Navbar from './Components/Navbar/Navbar'
+import About from './Components/About/About'
+import Experience from './Components/Experience/Experience'
+import Services from './Components/Services/Services'
+import Portfolio from './Components/Portfolio/Portfolio'
+import Testimonial from './Components/Testimonial/Testimonial'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Header />
+        <Navbar />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonial />
+        <Contact />
+        <Footer />
+    </>
   )
 }
 
