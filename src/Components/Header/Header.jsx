5677a76a7a3204e12e130af0917b193eb502920b
Lewis Murray <lewisdmurray00@gmail.com>
2022-07-06 18:48:46 +0100

    Added an image container for my Header section and then imported the buttons

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './header.css'
+import HeaderButtons from './HeaderButtons'
 
 const Header = () => {
   return (
@@ -8,6 +9,11 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Lewis Murray</h1>
         <h5 className='text-light'>Front End Developer</h5>
+        <HeaderButtons />
+
+        <div className='header__image__container'>
+          <img src='' alt='Myself'/>
+        </div>
       </div>
     </header>
   )
--- src/Components/Header/HeaderButtons.jsx
+++ src/Components/Header/HeaderButtons.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import CV from '../../Assests/Lewis-Murray-CV.pdf'
 
 const HeaderButtons = () => {
   return (
     <div className='HeaderButtons'>
-        <a href=''>Download CV</a>
-        <a href='#contact'>Contact Me!</a>
+        <a href={CV} download className='btn'>Download CV</a>
+        <a href='#contact' className='btn btn-primary'>Contact Me!</a>
     </div>
   )
 }
