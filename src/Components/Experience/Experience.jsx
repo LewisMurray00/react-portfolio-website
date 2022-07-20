a2549ced2cbba58b7270426084b13b063a7e2945
Lewis Murray <lewisdmurray00@gmail.com>
2022-07-20 16:28:18 +0100

    began creating the experience.jsx file

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+      
+      <h5>Skills</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container'>
+        
+        <div className='experience__frontend'>
+          
+        </div>
+      
+      </div>      
+    
     </section>
   )
 }
