78066749facd16e5bd99d077292bcbea712ad84c
Lewis Murray <lewisdmurray00@gmail.com>
2022-07-22 16:54:55 +0100

    FInished coding the message 'cards' for the contact.jsx section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {FaInstagram} from 'react-icons/fa'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
@@ -10,11 +13,30 @@ const Contact = () => {
       <div className='container contact__container'>
         
         <div className='contact__options'>
-          
+          <article className='contact__option'>
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>lewisdmurray00@gmail.com</h5>
+            <a href='mailto:lewisdmurray00@gmail.com' target="_blank">Send a message</a>
+          </article>
+
+          <article className='contact__option'>
+            <FaInstagram />
+            <h4>Instagram</h4>
+            <h5>lewismurrayy</h5>
+            <a href='https://www.instagram.com' target="_blank">Follow Me</a>
+          </article>
+
+          <article className='contact__option'>
+            <BsWhatsapp />
+            <h4>Whatsapp</h4>
+            <h5>+447577380000</h5>
+            <a href='https://api.whatsapp.com/send?phone=+447577380000' target="_blank">Send a message</a>
+          </article>
         </div>
 
         <form action=''>
-
+          
         </form>
       </div>
     </section>
